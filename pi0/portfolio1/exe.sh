a4628f4b9084246056055b15f08f010a33846e0c
nthom18 <nthom18@student.sdu.dk>
2020-10-25 14:44:15 +0000

    Edited exe.sh

--- pi0/portfolio1/exe.sh
+++ pi0/portfolio1/exe.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# Ctrl-C handler for clean shutdown
-shutdown()
-{
-  echo hello
-  exit 0
-}
+# Ctrl-C handler
+#shutdown()
+#{
+#  echo hello
+#  exit 0
+#}
 
-trap shutdown EXIT
+#trap shutdown EXIT
 
 # Compile and run in background
 gcc -Wall -o Light light.c -lwiringPi
