e0572eccdb925731687bcd85a1ad507418872921
Nicoline <nthom18@student.sdu.dk>
2021-03-19 13:35:09 +0100

    Added link to video that tests offb_postctl.py

--- Python simple simulator/behaviour.py
+++ Python simple simulator/behaviour.py
@@ -13,62 +13,62 @@ from vector import Vector2D
 from boid import Boid
 import constants
 
-ALIGN_FACTOR = 0.01
-COHESION_FACTOR = 0
-SEPERTION_FACTOR = 0
-
 class Behaviour():
 
-    def __init__(self, boid):
+    def __init__(self, boid, flock, a, c, s):
         self.boid = boid
+        self.percieved = []
 
-    def alignment(self, flock):
+        for flockmate in flock:
+            if flockmate.position.distance_to(self.boid.position) < self.boid.perception:
+                self.percieved.append(flockmate)
+
+        self.force = a * self.alignment() + c * self.cohesion() + s * self.separation()
+
+
+    def alignment(self):
         steering = Vector2D(*np.zeros(2))
         avg_vec = Vector2D(*np.zeros(2))
         total = 0
 
-        for flockmate in flock:
-            diff = [flockmate.position.x - self.boid.position.x, flockmate.position.y - self.boid.position.y]
-            if np.linalg.norm(diff) < self.boid.perception:
-                avg_vec += flockmate.velocity
-                total += 1
+        for flockmate in self.percieved:
+            avg_vec += flockmate.velocity
+            total += 1
         
         if total > 0:
             avg_vec /= total
-            avg_vec = (avg_vec /np.linalg.norm([avg_vec.x, avg_vec.y])) * constants.MAX_SPEED
+            avg_vec = (avg_vec / avg_vec.__abs__()) * constants.MAX_SPEED
             steering = avg_vec - self.boid.velocity
-
-        # if steering.__abs__() > constants.MAX_FORCE:
-        #     steering = (steering / steering.__abs__()) * constants.MAX_FORCE
 
         return steering
 
-    def cohesion(self, flock):
+
+    def cohesion(self):
+        steering = Vector2D(*np.zeros(2))
         center_of_mass = Vector2D(*np.zeros(2))
-        steering = [0,0]
         total = 0
 
-        for flockmate in flock:
-            if flockmate.position.distance_to(self.boid.position) < self.boid.perception:
-                center_of_mass += flockmate.position
-                total += 1
+        for flockmate in self.percieved:
+            center_of_mass += flockmate.position
+            total += 1
 
         if total > 0:
             center_of_mass /= total
-            vec_to_com = [center_of_mass.x - self.boid.position.x, center_of_mass.y - self.boid.position.y]
-            
-            if np.linalg.norm(vec_to_com) > 0:
-                vec_to_com = (vec_to_com / np.linalg.norm(vec_to_com)) * constants.MAX_SPEED
-            
-            steering = [vec_to_com[0] - self.boid.velocity.x, vec_to_com[1] - self.boid.velocity.y]
+            vec_to_com = center_of_mass - self.boid.position
 
-        return Vector2D(steering[0], steering[1])
+            if vec_to_com.__abs__() > 0:
+                vec_to_com = (vec_to_com / vec_to_com.__abs__()) * constants.MAX_SPEED
+            
+            steering = vec_to_com - self.boid.velocity
 
-    def separation(self, flock):
+        return steering
+
+
+    def separation(self):
         steering = Vector2D(*np.zeros(2))
         avg_vector = Vector2D(*np.zeros(2))
         total = 0
-        for flockmate in flock:
+        for flockmate in self.percieved:
             distance = flockmate.position.distance_to(self.boid.position)
             
             if self.boid.position != flockmate.position and distance < (self.boid.perception / 3):
